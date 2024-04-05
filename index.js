809188c50a6a509294b6d403baa1a2aba4a53716
Anil prajapati <its.anilprajapatii@gmail.com>
2024-04-05 11:16:29 +0530

    anil

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const fastify = fastifyRoot({});
 const bcrypt = require('bcrypt');
 const path = require('node:path');
 
+fastify.register(require('@fastify/cors'), (instance) => {
+  return (req, callback) => {
+    const corsOptions = {
+      // This is NOT recommended for production as it enables reflection exploits
+      origin: true
+    };
+
+    // do not include CORS headers for requests from localhost
+    if (/^localhost$/m.test(req.headers.origin)) {
+      corsOptions.origin = false
+    }
+
+    // callback expects two parameters: error and options
+    callback(null, corsOptions)
+  }
+})
+
 fastify.register(require("@fastify/view"), {
   engine: {
     ejs: require("ejs"),
