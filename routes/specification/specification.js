54b8c72ee2250a7f884276b06da361cbad517612
Anil prajapati <its.anilprajapatii@gmail.com>
2024-04-15 20:01:59 +0530

    naveed

--- routes/specification/specification.js
+++ routes/specification/specification.js
@@ -5,19 +5,33 @@ async function addSpecification(fastify, options) {
     const { name, keyname, keyType, ismandatory, isvariant, isfilter } = request.body;
     const specifications = {};
 
-    // Loop through each key name
-    for (let i = 0; i < keyname.length; i++) {
+    // Check if arrays are defined and not empty
+      let mandatory= false;
+      let filter= false;
+      let variant= false;
       
-      specifications[keyname[i]] = {
-        type: keyType[i],
-        required: ismandatory[i], // Convert string to boolean
-        isFilter: isfilter[i], // Convert string to boolean
-        isVariant: isvariant[i], // Convert string to boolean
-      };
-    }
+      // Loop through each key name
+      for (let i = 0; i < keyname.length; i++) {
+        if(ismandatory[i]=="true"){
+          mandatory = true;
+        }
+        if(isfilter[i]=="true"){
+          filter = true;
+        }
+        if(isvariant[i]=="true"){
+          variant = true;
+        }
+        specifications[keyname[i]] = {
+          type: keyType[i],
+          required: mandatory?true:false, // Convert string to boolean
+          isFilter: filter?true:false, // Convert string to boolean
+          isVariant: variant?true:false, // Convert string to boolean
+        };
+      }
+   
 
     const Spec = new Specification({
-      name: name,
+      name: "naveed",
       specification: specifications,
     });
     const BrandSaved = await Spec.save();
