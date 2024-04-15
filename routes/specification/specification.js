07cceaef583030add21a55ec684fe81765f573bb
Anil prajapati <its.anilprajapatii@gmail.com>
2024-04-15 18:03:46 +0530

    anil

--- routes/specification/specification.js
+++ routes/specification/specification.js
@@ -1,20 +1,23 @@
 const { Specification } = require("../../models/allModels");
+
 async function addSpecification(fastify, options) {
   fastify.post("/add-specifications", async (request, reply) => {
     const { name, keyname, keyType, ismandatory, isvariant, isfilter } = request.body;
     const specifications = {};
-    console.log(request.body);
+
+    // Loop through each key name
     for (let i = 0; i < keyname.length; i++) {
-      specifications[keyname] = {
-        type: keyType,
-        required: ismandatory?true:false,
-        isFilter:isfilter?true:false,
-        isVariant:isvariant?true:false,
+      specifications[keyname[i]] = {
+        type: keyType[i],
+        required: ismandatory[i]?true:false, // Convert string to boolean
+        isFilter: isfilter[i]?true:false, // Convert string to boolean
+        isVariant: isvariant[i]?true:false, // Convert string to boolean
       };
     }
+
     const Spec = new Specification({
       name: name,
-      specification:specifications,
+      specification: specifications,
     });
     const BrandSaved = await Spec.save();
     return { Spec };
