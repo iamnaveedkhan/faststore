82aec6b6fc9bf59e1342e3d4f74fd422c9602d3b
Anil prajapati <its.anilprajapatii@gmail.com>
2024-04-18 13:18:19 +0530

    anil

--- models/allModels.js
+++ models/allModels.js
@@ -1,151 +1,150 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, default: null },
-    mobile: { type: Number, unique: true },
-    longitude : { type : String, default: null},
-    latitude : { type : String, default: null},
-    role: { type: Number, default: 1 },
-    isActive : { type:Number, default : 1},
-    password:{type:String,default:"Zerotouch"}
+  name: { type: String, default: null },
+  mobile: { type: Number, unique: true },
+  longitude: { type: String, default: null },
+  latitude: { type: String, default: null },
+  role: { type: Number, default: 1 },
+  isActive: { type: Number, default: 1 },
+  password: { type: String, default: "Zerotouch" },
 });
 
 const addressesSchema = new mongoose.Schema({
-    address: { type: String, default: null },
-    city: { type: String, default: null },
-    state: { type: String, default: null },
-    user_id: { type: Schema.Types.ObjectId, ref: 'User' },
+  address: { type: String, default: null },
+  city: { type: String, default: null },
+  state: { type: String, default: null },
+  user_id: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
 const ordersSchema = new mongoose.Schema({
-    orderDate: { type: Date, default: Date.now },
-    status: { type: Number, default: 1 },
-    orderId: { type: String, default: null },
-    product: { type: Schema.Types.ObjectId, ref: 'Product' },
+  orderDate: { type: Date, default: Date.now },
+  status: { type: Number, default: 1 },
+  orderId: { type: String, default: null },
+  product: { type: Schema.Types.ObjectId, ref: "Product" },
 });
 
 const brandSchema = new mongoose.Schema({
-    brandName: { type: String, default: null },
-    brandImage: { type: String, default: null },
+  brandName: { type: String, default: null },
+  brandImage: { type: String, default: null },
 });
 
 const categorySchema = new mongoose.Schema({
-    categoryName: { type: String, default: null },
-    categoryImage: { type: String, default: null },
+  categoryName: { type: String, default: null },
+  categoryImage: { type: String, default: null },
 });
 
 const subCategorySchema = new mongoose.Schema({
-    subCategoryName: { type: String, default: null },
-    subCategoryImage: { type: String, default: null },
-    category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  subCategoryName: { type: String, default: null },
+  subCategoryImage: { type: String, default: null },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
 });
 
 const modelSchema = new mongoose.Schema({
-    productName: { type: String, default: null },
-    photo: { type : String, default: " " },
-    brand: { type: Schema.Types.ObjectId, ref: 'Brand' },
-    category: { type: Schema.Types.ObjectId, ref: 'Category' },
-    subCategory: { type: Schema.Types.ObjectId, ref: 'SubCategory' },
+  productName: { type: String, default: null },
+  photo: { type: String, default: " " },
+  brand: { type: Schema.Types.ObjectId, ref: "Brand" },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  subCategory: { type: Schema.Types.ObjectId, ref: "SubCategory" },
 });
 
 const model2Schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-      },
-    type: {
-        type: String,
-        required: true
+  groupId: {
+    type: String,
+    default: function () {
+      return Math.random().toString(36).substring(7);
     },
-    properties: {
-        category: String,
-        subcategory: String,
-        brand: String,
-        description: String,
-    },
-    photo: { type : String, default: " " },
-    specification: mongoose.Schema.Types.Mixed,
-  });
+  },
+  type: {
+    type: String,
+    default: null,
+  },
+  properties: {
+    category:{ type: String, default: null },
+    subcategory: { type: String, default: null },
+    vertical: { type: Schema.Types.ObjectId, ref: "Specification" },
+  },
+  photo: { type: [String], default: null },
+  specification: mongoose.Schema.Types.Mixed,
+});
 
-  const variantSchema = new mongoose.Schema({
-    model: { type: Schema.Types.ObjectId, ref: 'Model2' },
-    variants: {
-      color: String,
-      images: [String],
-      SKU: String,
-      price: Number,
-      quantity: Number,
-  }
-  })
+const variantSchema = new mongoose.Schema({
+  model: { type: Schema.Types.ObjectId, ref: "Model2" },
+  variants: {
+    color: String,
+    images: [String],
+    SKU: String,
+    price: Number,
+    quantity: Number,
+  },
+});
 
-  const product2Schema = new mongoose.Schema({
-    model: { type: Schema.Types.ObjectId, ref: 'Model2' },
-    user: {
-        _id: { type: 'string' },
-        shopNumber: { type: Number },
-        shopName: { type: String },
-    },
-    variants:  [{ type: Schema.Types.ObjectId, ref: 'Variant' }]
-  });
-
+const product2Schema = new mongoose.Schema({
+  model: { type: Schema.Types.ObjectId, ref: "Model2" },
+  user: {
+    _id: { type: "string" },
+    shopNumber: { type: Number },
+    shopName: { type: String },
+  },
+  variants: [{ type: Schema.Types.ObjectId, ref: "Variant" }],
+});
 
 const photoSchema = new mongoose.Schema({
-    model: { type: Schema.Types.ObjectId, ref: 'Model' },
-    photos: { type: [String] } // Array of strings representing URLs
+  model: { type: Schema.Types.ObjectId, ref: "Model" },
+  photos: { type: [String] }, // Array of strings representing URLs
 });
 
 const productSchema = new mongoose.Schema({
-    model: {
-        _id: { type: 'string' },
-        productName: { type: 'string' },
-        photo: { type: 'string' },
-        description: { type: 'string' },
-    },
-    brand: {
-        _id: { type: 'string' },
-        brandName: { type: 'string' },
-        brandImage: { type: 'string' }
-    },
-    user: {
-        _id: { type: 'string' },
-        shopNumber: { type: Number },
-        shopName: { type: String },
-    },
-    category: {
-        _id: { type: 'string' },
-        categoryName: { type: 'string' }
-    },
-    subCategory: {
-        _id: { type: 'string' },
-        subCategoryName: { type: 'string' },
-        category: { type: 'string' }
-    },
-    photos: { type: Schema.Types.ObjectId, ref: 'Photo' },
-    specifications: { type: 'object' }, // Allow any key-value pairs
-    price: { type: 'number' },
-    quantity: { type: 'number' },
-    productDate: { type: Date, default: Date.now },
+  model: {
+    _id: { type: "string" },
+    productName: { type: "string" },
+    photo: { type: "string" },
+    description: { type: "string" },
+  },
+  brand: {
+    _id: { type: "string" },
+    brandName: { type: "string" },
+    brandImage: { type: "string" },
+  },
+  user: {
+    _id: { type: "string" },
+    shopNumber: { type: Number },
+    shopName: { type: String },
+  },
+  category: {
+    _id: { type: "string" },
+    categoryName: { type: "string" },
+  },
+  subCategory: {
+    _id: { type: "string" },
+    subCategoryName: { type: "string" },
+    category: { type: "string" },
+  },
+  photos: { type: Schema.Types.ObjectId, ref: "Photo" },
+  specifications: { type: "object" }, // Allow any key-value pairs
+  price: { type: "number" },
+  quantity: { type: "number" },
+  productDate: { type: Date, default: Date.now },
 });
 
-
 const inquirySchema = new mongoose.Schema({
-    date:{ type: Date, default: Date.now },
-    customer: {
-        _id: { type: 'string' },
-        customerNumber: { type: Number },
-        customerName: { type: String },
-    },
-    shop: {
-        _id: { type: 'string' },
-        shopNumber: { type: Number },
-        shopName: { type: String },
-    },
-    product: {
-            _id: { type: 'string' },
-            productName: { type: 'string' },
-            photo: { type: 'string' },
-        },
+  date: { type: Date, default: Date.now },
+  customer: {
+    _id: { type: "string" },
+    customerNumber: { type: Number },
+    customerName: { type: String },
+  },
+  shop: {
+    _id: { type: "string" },
+    shopNumber: { type: Number },
+    shopName: { type: String },
+  },
+  product: {
+    _id: { type: "string" },
+    productName: { type: "string" },
+    photo: { type: "string" },
+  },
 });
 
 // const dimensions = new mongoose.Schema({
@@ -155,28 +154,41 @@ const inquirySchema = new mongoose.Schema({
 //     }
 // })
 
-
 const specificationSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    category: { type: Schema.Types.ObjectId, ref: 'Category' },
-    // subCategory: { type: Schema.Types.ObjectId, ref: 'SubCategory' },
-    specification: mongoose.Schema.Types.Mixed,
+  name: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  // subCategory: { type: Schema.Types.ObjectId, ref: 'SubCategory' },
+  specification: mongoose.Schema.Types.Mixed,
 });
-  
-  
-const User = mongoose.model('User', userSchema);
-const Address = mongoose.model('Address', addressesSchema);
-const Order = mongoose.model('Order', ordersSchema);
-const Product = mongoose.model('Product', productSchema);
-const Product2 = mongoose.model('Product2', product2Schema);
-const Brand = mongoose.model('Brand', brandSchema);
-const Category = mongoose.model('Category', categorySchema);
-const SubCategory = mongoose.model('SubCategory', subCategorySchema);
-const Model = mongoose.model('Model', modelSchema);
-const Model2 = mongoose.model('Model2', model2Schema);
-const Photo = mongoose.model('Photo',photoSchema);
-const Inquiry = mongoose.model('Inquiry',inquirySchema);
-const Variant = mongoose.model('Variant',variantSchema);
-const Specification = mongoose.model('Specification', specificationSchema);
 
-module.exports = { Specification, Variant, User, Brand, Category, SubCategory, Model, Address, Order, Product , Product2, Model2 , Photo, Inquiry};
+const User = mongoose.model("User", userSchema);
+const Address = mongoose.model("Address", addressesSchema);
+const Order = mongoose.model("Order", ordersSchema);
+const Product = mongoose.model("Product", productSchema);
+const Product2 = mongoose.model("Product2", product2Schema);
+const Brand = mongoose.model("Brand", brandSchema);
+const Category = mongoose.model("Category", categorySchema);
+const SubCategory = mongoose.model("SubCategory", subCategorySchema);
+const Model = mongoose.model("Model", modelSchema);
+const Model2 = mongoose.model("Model2", model2Schema);
+const Photo = mongoose.model("Photo", photoSchema);
+const Inquiry = mongoose.model("Inquiry", inquirySchema);
+const Variant = mongoose.model("Variant", variantSchema);
+const Specification = mongoose.model("Specification", specificationSchema);
+
+module.exports = {
+  Specification,
+  Variant,
+  User,
+  Brand,
+  Category,
+  SubCategory,
+  Model,
+  Address,
+  Order,
+  Product,
+  Product2,
+  Model2,
+  Photo,
+  Inquiry,
+};
--- routes/products/addProduct.js
+++ routes/products/addProduct.js
@@ -1,21 +1,31 @@
-fastify.post('/addmodel', async (req, reply) => {
+const {User} = require("../../models/allModels");
+fastify.post('/addproduct', { onRequest: [fastify.authenticate] }, async (req, reply) => {
     try {
-        // Parse form data
+        // const userId = req.user.userId._id;
+        // const user = await User.findById(userId);
+
         const data = {};
+        let photos = [];
         let fileName;
+
+        // if (!user) {
+        //     return reply.code(404).send({ error: "User not found" });
+        // }
+        
+        // data['user']['_id'] = userId._id;
+        // data['user']['shopName'] = userId.name;
+        // data['user']['shopNumber'] = userId.mobile;
 
         for await (const part of req.parts()) {
             if (part.file) {
-                // It's a file, save it
                 fileName = part.filename;
                 const filePath = path.join("public/image/", fileName);
                 const writableStream = fs.createWriteStream(filePath);
                 await part.file.pipe(writableStream);
-                data['photo'] = `public/image/${fileName}`;
+                photos.push = `public/image/${fileName}`;
+
             } else {
-                // Handle other form fields
-                if (part.fieldname.startsWith('variants[]')) {
-                  del  // It's a variant field
+                if (part.fieldname.startsWith('specification.')) {
                     data[part.fieldname] = part.value;
                 } else if (part.fieldname.startsWith('properties.')) {
                     // Convert string values to ObjectId
@@ -25,20 +35,14 @@ fastify.post('/addmodel', async (req, reply) => {
                 }
             }
         }
-
-        // Add variants data to the data object
-    
-
-        // Create a new model instance with the parsed data
+        data['photo'] = photos;
         const newModel = new Model2(data);
 
-        // Save the model to the database
         const savedModel = await newModel.save();
 
-        // Respond with the saved model
         return savedModel;
     } catch (error) {
-        // Handle errors
+
         console.error('Error adding model:', error);
         reply.code(500).send({ error: 'Internal Server Error' });
     }
