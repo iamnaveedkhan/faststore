78fb152446e8d48c1b92233f70a89c8fd645f3a2
Anil prajapati <its.anilprajapatii@gmail.com>
2024-04-21 10:00:08 +0530

    anil

--- routes/products/all_lead.js
+++ routes/products/all_lead.js
@@ -398,6 +398,25 @@ async function getProduct(fastify, options) {
     }
   );
 
+  fastify.get(
+    "/allinquiries",
+    { onRequest: [fastify.authenticate] },
+    async (req, reply) => {
+      try {
+        const existingData = await Model.find();
+
+        if (existingData.length > 0) {
+          reply.send(existingData);
+        } else {
+          reply.code(404).send({ error: "No data found" });
+        }
+      } catch (error) {
+        console.error(error);
+        reply.code(500).send({ error: "Internal server error" });
+      }
+    }
+  );
+
   fastify.get("/inquiry/:id", { onRequest: [fastify.authenticate] }, async (req, reply) => {
     try {
       const inquiryId = req.params.id;
