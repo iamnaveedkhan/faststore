d07222c1fcf71c8a19d9b93de8180d3cc297fb89
iamnaveekhan <nk78603@gmail.com>
2024-05-07 18:09:00 +0530

    naveed

--- routes/location/location.js
+++ routes/location/location.js
@@ -1,29 +1,15 @@
 
 const { User } = require("../../models/allModels");
 async function location(fastify, options) {
-  fastify.register(require("@fastify/multipart"));
   fastify.post(
     "/savelocation", 
     { onRequest: [fastify.authenticate] },
     async (req, reply) => {
       try {
-        const parts = req.parts();
+        const {latitude , longitude} = req.body;
         const userid = req.user.userId._id;
         console.log(userid);
         const userData = await User.findById({ _id: userid });
-
-        let latitude;
-        let longitude;
-
-        for await (const part of parts) {
-          if (part.type === "field" && part.fieldname == "latitude") {
-            latitude = part.value;
-            // hasFileField = true;
-          } else if (part.type === "field" && part.fieldname == "longitude") {
-            longitude = part.value;
-            // hasNameField = true;
-          }
-        }
 
         userData.latitude = latitude;
         userData.longitude = longitude;
