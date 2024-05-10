f6932e5b10082a885e0b369107f2c0474997708e
Anil prajapati <its.anilprajapatii@gmail.com>
2024-05-10 14:55:11 +0530

    anil

--- routes/liked-viewed/liked.js
+++ routes/liked-viewed/liked.js
@@ -4,55 +4,37 @@ async function getLiked(fastify, options) {
 
     // -----------post route---------------
 
-  fastify.post(
-    "/liked/:id",
-    { onRequest: [fastify.authenticate] },
-    async (req, reply) => {
-      try {
-        const { hasLiked } = req.body;
-        console.log("hasLiked:", hasLiked);
-
-        const userId = req.user.userId._id;
-
-        let user = await Liked.findOne({ user: userId });
-        if (!user) {
-          user = new Liked({ user: userId, liked: [] });
-        }
-
-        const requestedProductId = req.params.id;
-        const product = await Product.findById(requestedProductId);
-
-        if (!product) {
-          return reply.send({ error: "Product not found" });
-        }
-
-        const index = user.liked.indexOf(requestedProductId);
-
-        if (hasLiked === "true") {
-          if (index !== -1) {
-            user.liked.splice(index, 1);
-            await user.save();
-            reply.send({ message: "Product unliked successfully" });
-          } else {
-            reply.send({ error: "Product is not liked" });
+    fastify.post(
+      "/liked/:id",
+      { onRequest: [fastify.authenticate] },
+      async (req, reply) => {
+        try {
+          const userId = req.user.userId._id;
+          const requestedProductId = req.params.id;
+    
+          let user = await Liked.findOne({ user: userId });
+          if (!user) {
+            user = new Liked({ user: userId, liked: [] });
           }
-        } else if (hasLiked === "false") {
+    
+          const index = user.liked.indexOf(requestedProductId);
+    
           if (index === -1) {
             user.liked.push(requestedProductId);
             await user.save();
             reply.send({ message: "Product liked successfully" });
           } else {
-            reply.send({ error: "Product is already liked" });
+            user.liked.splice(index, 1);
+            await user.save();
+            reply.send({ message: "Product unliked successfully" });
           }
-        } else {
-          reply.send({ error: "please enter valid input !" });
+        } catch (error) {
+          console.error(error);
+          reply.code(500).send({ error: "Internal server error" });
         }
-      } catch (error) {
-        console.error(error);
-        reply.code(500).send({ error: "Internal server error" });
       }
-    }
-  );
+    );
+    
 
     // -----------get route---------------
 
