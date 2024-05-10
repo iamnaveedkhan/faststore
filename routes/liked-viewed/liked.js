6233188fe10107bdaae1bed3393be319d45fe3bc
Anil prajapati <its.anilprajapatii@gmail.com>
2024-05-10 11:10:09 +0530

    anil

--- routes/liked-viewed/liked.js
+++ routes/liked-viewed/liked.js
@@ -1,70 +1,72 @@
-const {User,Product,Liked } = require("../../models/allModels");
+const { User, Product, Liked } = require("../../models/allModels");
 
 async function getLiked(fastify, options) {
   fastify.post(
     "/liked/:id",
     { onRequest: [fastify.authenticate] },
     async (req, reply) => {
-        try {
-            const { hasLiked } = req.body; 
-            console.log("hasLiked:", hasLiked);
+      try {
+        const { hasLiked } = req.body;
+        console.log("hasLiked:", hasLiked);
 
-            const userId = req.user.userId._id;
+        const userId = req.user.userId._id;
 
-            let user = await Liked.findOne({ user: userId });
-            if (!user) {
-                user = new Liked({ user: userId, liked: [] });
-            }
-
-            const requestedProductId = req.params.id;
-            const product = await Product.findById(requestedProductId);
-
-            if (!product) {
-                return reply.send({ error: "Product not found" });
-            }
-
-            const index = user.liked.indexOf(requestedProductId);
-
-            if (hasLiked === "true") {
-                if (index !== -1) {
-                    user.liked.splice(index, 1);
-                    await user.save();
-                    reply.send({ message: "Product unliked successfully" });
-                } else {
-                    reply.send({ error: "Product is not liked" });
-                }
-            } else if (hasLiked === "false") {
-                if (index === -1) {
-                    user.liked.push(requestedProductId);
-                    await user.save();
-                    reply.send({ message: "Product liked successfully" });
-                } else {
-                    reply.send({ error: "Product is already liked" });
-                }
-            } else {
-                reply.send({ error: "please enter valid input !" });
-            }
-        } catch (error) {
-            console.error(error);
-            reply.code(500).send({ error: "Internal server error" });
+        let user = await Liked.findOne({ user: userId });
+        if (!user) {
+          user = new Liked({ user: userId, liked: [] });
         }
+
+        const requestedProductId = req.params.id;
+        const product = await Product.findById(requestedProductId);
+
+        if (!product) {
+          return reply.send({ error: "Product not found" });
+        }
+
+        const index = user.liked.indexOf(requestedProductId);
+
+        if (hasLiked === "true") {
+          if (index !== -1) {
+            user.liked.splice(index, 1);
+            await user.save();
+            reply.send({ message: "Product unliked successfully" });
+          } else {
+            reply.send({ error: "Product is not liked" });
+          }
+        } else if (hasLiked === "false") {
+          if (index === -1) {
+            user.liked.push(requestedProductId);
+            await user.save();
+            reply.send({ message: "Product liked successfully" });
+          } else {
+            reply.send({ error: "Product is already liked" });
+          }
+        } else {
+          reply.send({ error: "please enter valid input !" });
+        }
+      } catch (error) {
+        console.error(error);
+        reply.code(500).send({ error: "Internal server error" });
+      }
     }
-);
-
-
+  );
 
   fastify.get(
     "/get-liked-products",
     { onRequest: [fastify.authenticate] },
     async (req, reply) => {
       try {
-        const user = await User.findOne({ _id: req.user.userId._id });
+        
+        const userId = req.user.userId._id;
+        console.log(userId);
 
+        const user = await Liked.findOne({user:userId})
+        
         const likedProductIds = user.liked;
 
         const likedProducts = await Product.find({
           _id: { $in: likedProductIds },
-        }).populate("user");
+        }).populate('user');
 
         reply.send(likedProducts);
       } catch (error) {
@@ -73,6 +75,7 @@ async function getLiked(fastify, options) {
       }
     }
   );
+  
 }
 
 module.exports = getLiked;
