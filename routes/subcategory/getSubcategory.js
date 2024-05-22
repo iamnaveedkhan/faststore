b3e08f39b1e1a163dac5dd88f615ea7f8d0a0495
Anil prajapati <its.anilprajapatii@gmail.com>
2024-05-22 15:10:52 +0530

    sub-categories/:id route

--- routes/subcategory/getSubcategory.js
+++ routes/subcategory/getSubcategory.js
@@ -19,6 +19,25 @@ async function getSubcategory(fastify, options) {
           }
         }
       );
+
+      fastify.get(
+        "/sub-categories/:id",
+        { onRequest: [fastify.authenticate] },
+        async (req, reply) => {
+          try {
+            const subcategoryId = req.params.id;
+            const existingSubCategory = await SubCategory.findOne({ _id: subcategoryId });
+            if (existingSubCategory) {
+              reply.send(existingSubCategory);
+            } else {
+              reply.code(404).send({ error: "User not found" });
+            }
+          } catch (error) {
+            console.error(error);
+            reply.code(500).send({ error: "Internal server error" });
+          }
+        }
+      );
 }
 
 module.exports = getSubcategory;
