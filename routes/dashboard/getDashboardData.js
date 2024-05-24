a3b6fa9870fbd9a65e7e667e527aad4ce39b17e0
Anil prajapati <its.anilprajapatii@gmail.com>
2024-05-24 17:47:08 +0530

    anil

--- routes/dashboard/getDashboardData.js
+++ routes/dashboard/getDashboardData.js
@@ -20,16 +20,14 @@ async function getDashboardData(fastify, options) {
   );
 
   fastify.get(
-    "/customers-count",
+    "/users-count",
     { onRequest: [fastify.authenticate] },
     async (req, reply) => {
       try {
-        const customerData = await User.find({role:2});
-
         const now = DateTime.local();
         const timeZone = "Asia/Kolkata";
         const nowIST = now.setZone(timeZone);
-
+  
         const todayInterval = Interval.fromDateTimes(
           nowIST.startOf("day"),
           nowIST.endOf("day")
@@ -43,31 +41,65 @@ async function getDashboardData(fastify, options) {
           nowIST.endOf("month")
         );
 
-        const [todayCount, weekCount, monthCount] = await Promise.all([
-          customerData.countDocuments({
+        console.log(nowIST.startOf("month"));
+        console.log(nowIST.endOf("month"));
+  
+        const [todayCustomerCount, weekCustomerCount, monthCustomerCount, todayRetailerCount, weekRetailerCount, monthRetailerCount] = await Promise.all([
+          User.countDocuments({
+            role: 1,
             date: {
               $gte: todayInterval.start.toJSDate(),
               $lte: todayInterval.end.toJSDate(),
             },
           }),
-          customerData.countDocuments({
+          User.countDocuments({
+            role: 1,
             date: {
               $gte: weekInterval.start.toJSDate(),
               $lte: weekInterval.end.toJSDate(),
             },
           }),
-          customerData.countDocuments({
+          User.countDocuments({
+            role: 1,
+            date: {
+              $gte: monthInterval.start.toJSDate(),
+              $lte: monthInterval.end.toJSDate(),
+            },
+          }),
+          User.countDocuments({
+            role: 2,
+            date: {
+              $gte: todayInterval.start.toJSDate(),
+              $lte: todayInterval.end.toJSDate(),
+            },
+          }),
+          User.countDocuments({
+            role: 2,
+            date: {
+              $gte: weekInterval.start.toJSDate(),
+              $lte: weekInterval.end.toJSDate(),
+            },
+          }),
+          User.countDocuments({
+            role: 2,
             date: {
               $gte: monthInterval.start.toJSDate(),
               $lte: monthInterval.end.toJSDate(),
             },
           }),
         ]);
-
+  
         return {
-          todayCount,
-          weekCount,
-          monthCount,
+          customer: {
+            todayAddedCustomerCount: todayCustomerCount,
+            weekAddedCustomerCount: weekCustomerCount,
+            monthAddedCustomerCount: monthCustomerCount,
+          },
+          retailer: {
+            todayAddedRetailerCount: todayRetailerCount,
+            weekAddedRetailerCount: weekRetailerCount,
+            monthAddedRetailerCount: monthRetailerCount,
+          },
         };
       } catch (error) {
         console.error(error);
@@ -75,61 +107,7 @@ async function getDashboardData(fastify, options) {
       }
     }
   );
-
-  fastify.get(
-    "/retailers-count",
-    { onRequest: [fastify.authenticate] },
-    async (req, reply) => {
-      try {
-        const now = DateTime.local();
-        const timeZone = "Asia/Kolkata";
-        const nowIST = now.setZone(timeZone);
-
-        const todayInterval = Interval.fromDateTimes(
-          nowIST.startOf("day"),
-          nowIST.endOf("day")
-        );
-        const weekInterval = Interval.fromDateTimes(
-          nowIST.startOf("week").set({ weekday: 1 }),
-          nowIST.endOf("week")
-        );
-        const monthInterval = Interval.fromDateTimes(
-          nowIST.startOf("month"),
-          nowIST.endOf("month")
-        );
-
-        const [todayCount, weekCount, monthCount] = await Promise.all([
-          User.countDocuments({
-            date: {
-              $gte: todayInterval.start.toJSDate(),
-              $lte: todayInterval.end.toJSDate(),
-            },
-          }),
-          User.countDocuments({
-            date: {
-              $gte: weekInterval.start.toJSDate(),
-              $lte: weekInterval.end.toJSDate(),
-            },
-          }),
-          User.countDocuments({
-            date: {
-              $gte: monthInterval.start.toJSDate(),
-              $lte: monthInterval.end.toJSDate(),
-            },
-          }),
-        ]);
-
-        return {
-          todayCount,
-          weekCount,
-          monthCount,
-        };
-      } catch (error) {
-        console.error(error);
-        reply.code(500).send({ error: "Internal server error" });
-      }
-    }
-  );
+  
 }
 
 module.exports = getDashboardData;
